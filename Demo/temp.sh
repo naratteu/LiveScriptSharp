a376aad10d07cbc90c7742eb88bf2a74ab187072
naratteu <skfhddlg@naver.com>
2025-12-31 04:23:36 +0900

    dotnet workload restore

--- Demo/temp.sh
+++ Demo/temp.sh
@@ -1,5 +1,6 @@
 rm -rf temp
 npm create vite@latest -- temp -t react-ts --no-interactive
+dotnet workload restore ../Wasm/*
 dotnet build ../Wasm -o temp/bin
 cat <<EOF > temp/bin/AppBundle/package.json
 { "type":"module", "name":"dotnet" }

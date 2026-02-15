a8264529694ff303f3802fdb458da5b586a6a60d
naratteu <skfhddlg@naver.com>
2026-02-15 12:13:58 +0900

    antigravity: livescript의 분해할당 기능을 c# 코드로 트랜스파일링 하기위해서, Test쪽에서는 테스트용 분해할당 예시코드를 넣어주시고, Core쪽에는 분해할당에 적절한 c# 코드가 생성되도록 만들어주세요

--- Core/Asts/Assign.cs
+++ Core/Asts/Assign.cs
@@ -6,6 +6,41 @@ partial record Assign
     {
         switch (this)
         {
+            case ((Obj or Arr) and { } left, "=", "=", { } right):
+                var tmp = "ref_" + Random.Shared.Next();
+                yield return $"var {tmp}=";
+                foreach (var r in right.Cat())
+                    yield return r;
+                yield return ";";
+                if (left is Obj obj)
+                {
+                    foreach (var item in obj.items)
+                    {
+                        if (item is Prop prop)
+                        {
+                            var key = prop.key switch
+                            {
+                                Key k => k.name,
+                                null when prop.val is Var v => v.value,
+                                _ => throw new Exception("Invalid destructuring key")
+                            };
+                            var nestedAssign = new Assign(prop.val, "=", "=", new Literal($"{tmp}.{key}"));
+                            foreach (var s in nestedAssign.Cat()) yield return s;
+                            yield return ";";
+                        }
+                    }
+                }
+                else if (left is Arr arr)
+                {
+                    var i = 0;
+                    foreach (var item in arr.items)
+                    {
+                        var nestedAssign = new Assign(item, "=", "=", new Literal($"{tmp}[{i++}]"));
+                        foreach (var s in nestedAssign.Cat()) yield return s;
+                        yield return ";";
+                    }
+                }
+                break;
             case ({ } left, "=", "=", { } right):
                 foreach (var l in left.Cat())
                     yield return l;
---
+++ Test/Destructuring.ls
@@ -0,0 +1,6 @@
+{a, b} = {a: 10, b: 20}
+console.log a
+console.log b
+[c, d] = [30, 40]
+console.log c
+console.log d
